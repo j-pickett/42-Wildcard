cea3c8ca4f437cb4affeea65f53f6abe87bdd294
j-pickett <pj259166@gmail.com>
2018-12-17 17:51:25 -0800

    updating test file with more commands/tests

--- test.py
+++ test.py
@@ -1,14 +1,27 @@
 import discord
 import asyncio
 import time
+import discord.ext import commands
 
 from discord.utils import get
 
 client = discord.Client()
+bot = commands.Bot(command_prefix = '!')
 
 token = 'NTIwNzA4OTMzMjQxNzk4Njc2.DvBsqg.ckPls4nvuiPVojYyjCECb5ilPaw'
 
 @client.event
+
+#COMMAND TO SET GAME NAME & MESSAGE IN TERMINAL IF ONLINE
+async def ready():
+	print('Online ready to kill')
+	await client.change_presence(game=discord.Game(name='In the Shower'))
+
+#COMMAND TO ADD ROLE TO SOMEONE AS THEY JOIN YOUR SERVER
+async def on_join(member):
+	role = discord.utils.get(member.server.role, name='ROLE')
+	await bot.add_roles(member, role)
+
 async def on_message(message):
 #CANT TALK TO ITSELF
 	if message.author == client.user:
@@ -25,6 +38,13 @@ async def on_message(message):
 
 #ADMIN COMMANDS THAT ONLY USERS IN ADMIN ROLE CAN USE
 bypass_list = [""]
+
+	if message.content.startswith('!echo'):
+		output = ''
+		for word in message.content.split()[1:]:
+			output += word
+			output += ' '
+		await client/send_message(message.channel, output)
 
 	if '!ANDREW' in message.content.upper():
 		myid = '<@295279188334739457>'
@@ -59,5 +79,15 @@ bypass_list = [""]
 	#if ':GayBoi:' in message.content:
 	#	emoji = get(client.get_all_emojis(), name=':GayBoi:')
 	#	await client.add_reaction(message, emoji)
+
+
+#TO MAKE A COMMAND USE THE .COMMAND AND CREATE THE FUNCTION & PARAMS
+#THIS FUNCTION NEEDS TO HAVE ADDED E-CHECKS AND MORE OPTIONS I.E(DELETE BASED ON ID OF USER ETC.)
+@bot.command(pass_context=True)
+async def clear(ctx, amount=100):
+	messages = []
+	async for message in client.logs_from(ctx.message.channel, limit=int(amount) + 1):
+		messages.append(message)
+	await client.delete_messages(messages)
 
 client.run(token)
